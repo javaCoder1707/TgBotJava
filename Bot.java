56b2bb2b8dcbe6aaecda9841d0af0d3ed452bf9b
Tigran Oganesyan <66678052+javaCoder1707@users.noreply.github.com>
2020-07-12 21:14:52 +0400

    Update Bot.java 2.1

--- Bot.java
+++ Bot.java
@@ -14,6 +14,7 @@ import uk.parser.ParserUK;
 import uk.parser.ResultUK;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
@@ -49,18 +50,19 @@ public class Bot extends TelegramLongPollingBot {
 
             case "/Russian":
             case "Привет":
+            case "привет":
                 sendMsg(message,
-                        "Привет\uD83D\uDD90, чтобы узнать новости на разные темы введите команду /newsAllR" +
+                        "Привет\uD83D\uDD90 чтобы узнать новости на разные темы введите команду /newsAllR" +
                                 "\n" + "\n" +
-                                "Чтобы узнать новости на тему политики \n \uD83C\uDFDB, введите команду /policyR\n" +
+                                "Чтобы узнать новости на тему политики \n \uD83C\uDFDB введите команду /policyR\n" +
                                 "\n" +
-                                "Чтобы узнать новости на тему электроники \uD83D\uDCF1, введите команду /mobileR\n" +
+                                "Чтобы узнать новости на тему электроники \uD83D\uDCF1 введите команду /mobileR\n" +
                                 "\n" +
-                                "Чтобы узнать новости на тему авто\n \uD83D\uDE98, введите команду /carR\n" +
+                                "Чтобы узнать новости на тему авто\n \uD83D\uDE98 введите команду /carR\n" +
                                 "\n" +
-                                "Чтобы узнать новости на тему спорта \n⚽️, введите команду /sportR\n" +
+                                "Чтобы узнать новости на тему спорта \n ⚽️ введите команду /sportR\n" +
                                 "\n" +
-                                "Чтобы узнать новости на тему искусства \uD83C\uDFA8, введите команду /artR\n" +
+                                "Чтобы узнать новости на тему искусства \n \uD83C\uDFA8 введите команду /artR\n" +
                                 "\n" +
                                 "Если возникли неполадки введите команду - /helpR");
                 break;
@@ -68,7 +70,7 @@ public class Bot extends TelegramLongPollingBot {
             case "/newsAllR":
                 try {
                     ParserRU.allNewsResults().forEach(x -> sendMsg(message, x.toString()));
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -79,11 +81,11 @@ public class Bot extends TelegramLongPollingBot {
                     List<ResultRU> results = ParserRU.policyNewsResults();
 
                     for (int i = 0; i < 5 ; i++) {
-                        Thread.sleep(1000);
                         sendMsg(message, results.get(i).toString());
+                        Thread.sleep(1000);
                     }
 
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
 
                 } catch (IOException | InterruptedException e) {
                     e.printStackTrace();
@@ -95,11 +97,11 @@ public class Bot extends TelegramLongPollingBot {
                     List<ResultRU> results = ParserRU.mobileNewsResults();
 
                     for (int i = 0; i < 5; i++) {
-                        Thread.sleep(1000);
                         sendMsg(message, results.get(i).toString());
+                        Thread.sleep(1000);
                     }
 
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
 
                 } catch(IOException | InterruptedException e){
                     e.printStackTrace();
@@ -111,11 +113,11 @@ public class Bot extends TelegramLongPollingBot {
                     List<ResultRU> results = ParserRU.carsNewsResults();
 
                     for (int i = 0; i < 5; i++) {
-                        Thread.sleep(1000);
                         sendMsg(message, results.get(i).toString());
+                        Thread.sleep(1000);
                     }
 
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
 
                 } catch(IOException | InterruptedException e){
                     e.printStackTrace();
@@ -127,11 +129,11 @@ public class Bot extends TelegramLongPollingBot {
                     List<ResultRU> results = ParserRU.sportNewsResults();
 
                     for (int i = 0; i < 5; i++) {
-                        Thread.sleep(1000);
                         sendMsg(message, results.get(i).toString());
+                        Thread.sleep(1000);
                     }
 
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
 
                 } catch(IOException | InterruptedException e){
                     e.printStackTrace();
@@ -143,11 +145,11 @@ public class Bot extends TelegramLongPollingBot {
                     List<ResultRU> results = ParserRU.artNewsResults();
 
                     for (int i = 0; i < 5; i++) {
-                        Thread.sleep(1000);
                         sendMsg(message, results.get(i).toString());
+                        Thread.sleep(1000);
                     }
 
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
 
                 } catch(IOException | InterruptedException e){
                     e.printStackTrace();
@@ -158,6 +160,8 @@ public class Bot extends TelegramLongPollingBot {
                 sendMsg(message,  "Приносим извинение за неполадки, программа находиться в стадии разработки " +
                         "\n" + "\n" +
                         "Если вы нашли неполадки или хотите расширить функциональность программы пишите на почту javacoder1707@gmail.com");
+
+                System.out.println("HELP: Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
                 break;
 
 
@@ -169,25 +173,25 @@ public class Bot extends TelegramLongPollingBot {
             case "Hi":
             case "hello":
                 sendMsg(message,
-                        "Hi\uD83D\uDD90, to get the all news, enter the command \n /newsAll" +
+                        "Hi\uD83D\uDD90 to get the all news, enter the command \n /newsAll" +
                                 "\n" + "\n" +
-                                "To get news on the topic of UK \uD83C\uDDEC\uD83C\uDDE7, enter the command /newsUK \n" +
+                                "To get news on the topic of UK \uD83C\uDDEC\uD83C\uDDE7 enter the command /newsUK \n" +
                                 "\n" +
-                                "To get news on the topic of world \uD83C\uDF0D, enter the command /newsWorld \n" +
+                                "To get news on the topic of world \uD83C\uDF0D enter the command /newsWorld \n" +
                                 "\n" +
-                                "To get news about politic \uD83C\uDFDB, enter the command /politic \n" +
+                                "To get news about politic \uD83C\uDFDB enter the command /politic \n" +
                                 "\n" +
-                                "To get news about cars \uD83D\uDE98, enter the command /motors \n" +
+                                "To get news about cars \uD83D\uDE98 enter the command /motors \n" +
                                 "\n" +
-                                "To get news about the sport \uD83C\uDFC8, enter the command /sport \n" +
+                                "To get news about the sport ⚽ enter the command /sport \n" +
                                 "\n" +
-                                "If you have problems, enter the command\n - /help");
+                                "If you have problems enter the command\n - /help");
                 break;
 
             case "/newsAll":
                 try {
                     ParserUK.allNewsResults().forEach(x -> sendMsg(message, x.toString()));
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -198,11 +202,11 @@ public class Bot extends TelegramLongPollingBot {
                     List<ResultUK> results = ParserUK.newsOfUKResults();
 
                     for(int i = 0; i < 5; i++){
-                        Thread.sleep(1000);
                         sendMsg(message, results.get(i).toString());
+                        Thread.sleep(1000);
                     }
 
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
 
                 } catch (IOException | InterruptedException e){
                     e.printStackTrace();
@@ -214,11 +218,11 @@ public class Bot extends TelegramLongPollingBot {
                     List<ResultUK> results = ParserUK.worldNewsResults();
 
                     for(int i = 0; i < 5; i++){
-                        Thread.sleep(1000);
                         sendMsg(message, results.get(i).toString());
+                        Thread.sleep(1000);
                     }
 
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
 
                 } catch (IOException | InterruptedException e) {
                     e.printStackTrace();
@@ -230,11 +234,11 @@ public class Bot extends TelegramLongPollingBot {
                     List<ResultUK> results = ParserUK.politicNewsResults();
 
                     for(int i = 0; i < 5; i++){
-                        Thread.sleep(1000);
                         sendMsg(message, results.get(i).toString());
+                        Thread.sleep(1000);
                     }
 
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
 
                 } catch (IOException | InterruptedException e) {
                     e.printStackTrace();
@@ -246,11 +250,11 @@ public class Bot extends TelegramLongPollingBot {
                     List<ResultUK> results = ParserUK.motorsNewsResults();
 
                     for(int i = 0; i < 5; i++){
-                        Thread.sleep(1000);
                         sendMsg(message, results.get(i).toString());
+                        Thread.sleep(1000);
                     }
 
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
 
                 } catch (IOException | InterruptedException e) {
                     e.printStackTrace();
@@ -262,11 +266,11 @@ public class Bot extends TelegramLongPollingBot {
                     List<ResultUK> results = ParserUK.sportNewsResults();
 
                     for(int i = 0; i < 5; i++){
-                        Thread.sleep(1000);
                         sendMsg(message, results.get(i).toString());
+                        Thread.sleep(1000);
                     }
 
-                    System.out.println("Sent successfully");
+                    System.out.println("Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
 
                 } catch (IOException | InterruptedException e) {
                     e.printStackTrace();
@@ -278,6 +282,8 @@ public class Bot extends TelegramLongPollingBot {
                         "We apologize for the problems, the program is under development" +
                                 "\n" + "\n" +
                                 "If you find a problem or want to expand the functionality of the program, write to javacoder1707@gmail.com");
+
+                System.out.println("HELP: Sent successfully to " + message.getFrom().getFirstName() + " " + message.getFrom().getLastName() + " at " + new Date());
                 break;
 
             default:
@@ -285,6 +291,7 @@ public class Bot extends TelegramLongPollingBot {
                 sendMsg(message, "Enter /start");
                 break;
         }
+        message.getNewChatMembers().forEach(x -> System.out.println(x.getFirstName() + " " + x.getLastName()));
     }
 
     @Override
@@ -297,3 +304,4 @@ public class Bot extends TelegramLongPollingBot {
         return "1237517329:AAHOKBf9a3XulgmsR2pBF-GHmnuv_4hkgCA";
     }
 }
+
