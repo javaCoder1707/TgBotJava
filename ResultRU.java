02205d667f2dc46635c2c52be6afa17959b98ccf
Tigran Oganesyan <66678052+javaCoder1707@users.noreply.github.com>
2020-07-27 14:39:37 +0400

    Update ResultRU.java  1.1

--- ResultRU.java
+++ ResultRU.java
@@ -3,10 +3,12 @@ package ru.parser;
 public class ResultRU {
     private String url;
     private String text;
+    private String type;
 
-    public ResultRU(String url, String text){
+    public ResultRU(String url, String text, String type){
         this.url = url;
         this.text = text;
+        this.type = type;
     }
 
     public String getUrl() {
@@ -27,6 +29,8 @@ public class ResultRU {
 
     @Override
     public String toString() {
+        if(this.type.equals("Sport")) return this.text + "\n" + "https://www.sports.ru/news" + this.url;
+
         return this.text + "\n" + "https://yandex.ru" + this.url;
     }
 }
