2f29d3960649656e875feaabe27ebb54154541cd
Tigran Oganesyan <66678052+javaCoder1707@users.noreply.github.com>
2020-07-29 00:52:04 +0400

    Update ParserUK.java 2.0

--- ParserUK.java
+++ ParserUK.java
@@ -3,12 +3,12 @@ package uk.parser;
 import org.apache.http.util.TextUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class ParserUK {
     public static List<ResultUK> receiveAllNewsResults() throws IOException {
@@ -17,17 +17,16 @@ public class ParserUK {
         System.out.println("Parsing...");
         Document document = Jsoup.connect("https://www.thesun.co.uk/news/").get();
 
-        Elements aLinks = document.getElementsByClass("rail__item-anchor");
-        Elements headers = document.getElementsByClass("rail__item-headline theme__copy-color");
-        AtomicInteger i = new AtomicInteger(0);
+        Elements divs = document.getElementsByClass("rail__item swiper-slide");
 
-        aLinks.forEach(a -> {
-            String url = (TextUtils.isEmpty(a.attr("href"))) ? "https://www.thesun.co.uk/news/" : a.attr("href");
-            String text = (TextUtils.isEmpty(headers.get(i.get()).text())) ? "News" : headers.get(i.get()).text();
+        divs.forEach(div -> {
+            Element aLink = div.child(3);
+            Element header = div.child(1).child(0);
+
+            String url = (TextUtils.isEmpty(aLink.attr("href"))) ? "https://www.thesun.co.uk/news/" : aLink.attr("href");
+            String text = (TextUtils.isEmpty(header.text())) ? "News" : header.text();
 
             results.add(new ResultUK(url, text));
-
-            i.getAndIncrement();
         });
 
         return results;
@@ -39,17 +38,15 @@ public class ParserUK {
         System.out.println("Parsing...");
         Document document = Jsoup.connect("https://www.thesun.co.uk/news/uknews/").get();
 
-        Elements aLinks = document.getElementsByClass("teaser-anchor");
-        Elements headers = document.getElementsByClass("teaser__headline theme__copy-color");
-        AtomicInteger i = new AtomicInteger(0);
+        Elements aLinks = document.getElementsByClass("text-anchor-wrap");
 
-        aLinks.forEach(a -> {
-            String url = (TextUtils.isEmpty(a.attr("href"))) ? "https://www.thesun.co.uk/news/uknews/" : a.attr("href");
-            String text = (TextUtils.isEmpty(headers.get(i.get()).text())) ? "News of UK" : headers.get(i.get()).text();
+        aLinks.forEach(aLink -> {
+            Element header = aLink.child(0);
+
+            String url = (TextUtils.isEmpty(aLink.attr("href"))) ? "https://www.thesun.co.uk/news/uknews/" : aLink.attr("href");
+            String text = (TextUtils.isEmpty(header.text())) ? "News of UK" : header.text();
 
             results.add(new ResultUK(url, text));
-
-            i.getAndIncrement();
         });
 
 
@@ -62,17 +59,15 @@ public class ParserUK {
         System.out.println("Parsing...");
         Document document = Jsoup.connect("https://www.thesun.co.uk/news/worldnews/").get();
 
-        Elements aLinks = document.getElementsByClass("teaser-anchor");
-        Elements headers = document.getElementsByClass("teaser__headline theme__copy-color");
-        AtomicInteger i = new AtomicInteger(0);
+        Elements aLinks = document.getElementsByClass("text-anchor-wrap");
 
         aLinks.forEach(aLink -> {
+            Element header = aLink.child(0);
+
             String url = (TextUtils.isEmpty(aLink.attr("href"))) ? "https://www.thesun.co.uk/news/worldnews/" : aLink.attr("href");
-            String text = (TextUtils.isEmpty(headers.get(i.get()).text())) ? "World News" : headers.get(i.get()).text();
+            String text = (TextUtils.isEmpty(header.text())) ? "World News" : header.text();
 
             results.add(new ResultUK(url, text));
-
-            i.getAndIncrement();
         });
 
         return results;
@@ -84,17 +79,15 @@ public class ParserUK {
         System.out.println("Parsing...");
         Document document = Jsoup.connect("https://www.thesun.co.uk/news/politics/").get();
 
-        Elements aLinks = document.getElementsByClass("teaser-anchor");
-        Elements headers = document.getElementsByClass("teaser__headline theme__copy-color");
-        AtomicInteger i = new AtomicInteger(0);
+        Elements aLinks = document.getElementsByClass("text-anchor-wrap");
 
         aLinks.forEach(aLink -> {
+            Element header = aLink.child(0);
+
             String url = (TextUtils.isEmpty(aLink.attr("href"))) ? "https://www.thesun.co.uk/news/politics/" : aLink.attr("href");
-            String text = (TextUtils.isEmpty(headers.get(i.get()).text())) ? "Politic News" : headers.get(i.get()).text();
+            String text = (TextUtils.isEmpty(header.text())) ? "Politic News" : header.text();
 
             results.add(new ResultUK(url, text));
-
-            i.getAndIncrement();
         });
 
         return results;
@@ -106,17 +99,15 @@ public class ParserUK {
         System.out.println("Parsing...");
         Document document = Jsoup.connect("https://www.thesun.co.uk/motors/").get();
 
-        Elements aLinks = document.getElementsByClass("teaser-anchor");
-        Elements headers = document.getElementsByClass("teaser__headline theme__copy-color");
-        AtomicInteger i = new AtomicInteger(0);
+        Elements aLinks = document.getElementsByClass("text-anchor-wrap");
 
         aLinks.forEach(aLink -> {
+            Element header = aLink.child(0);
+
             String url = (TextUtils.isEmpty(aLink.attr("href"))) ? "https://www.thesun.co.uk/motors/" : aLink.attr("href");
-            String text = (TextUtils.isEmpty(headers.get(i.get()).text())) ? "Motors" : headers.get(i.get()).text();
+            String text = (TextUtils.isEmpty(header.text())) ? "Motors" : header.text();
 
             results.add(new ResultUK(url, text));
-
-            i.getAndIncrement();
         });
 
         return results;
@@ -128,21 +119,18 @@ public class ParserUK {
         System.out.println("Parsing...");
         Document document = Jsoup.connect("https://www.thesun.co.uk/sport/").get();
 
-        Elements aLinks = document.getElementsByClass("teaser-anchor");
-        Elements headers = document.getElementsByClass("teaser__headline theme__copy-color");
-        AtomicInteger i = new AtomicInteger(0);
+        Elements divs = document.getElementsByClass("rail__item swiper-slide");
 
-        aLinks.forEach(aLink -> {
+        divs.forEach(div -> {
+            Element aLink = div.child(3);
+            Element header = div.child(1).child(0);
+
             String url = (TextUtils.isEmpty(aLink.attr("href"))) ? "https://www.thesun.co.uk/sport/" : aLink.attr("href");
-            String text = (TextUtils.isEmpty(headers.get(i.get()).text())) ? "Sport" : headers.get(i.get()).text();
+            String text = (TextUtils.isEmpty(header.text())) ? "Sport" : header.text();
 
            results.add(new ResultUK(url, text));
-
-            i.getAndIncrement();
         });
 
         return results;
     }
-
-
 }
