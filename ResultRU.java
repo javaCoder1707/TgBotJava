4d35596e5a09969ae2e831661bc7f51f4073ac89
Tigran Oganesyan <66678052+javaCoder1707@users.noreply.github.com>
2020-08-04 19:00:29 +0400

    Update ResultRU.java

--- ResultRU.java
+++ ResultRU.java
@@ -19,18 +19,10 @@ public class ResultRU {
         return text;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
     @Override
     public String toString() {
         if(this.type.equals("Sport")) return this.text + "\n" + "https://www.sports.ru/news" + this.url;
 
-        return this.text + "\n" + "https://yandex.ru" + this.url;
+        return this.text + "\n" + this.url;
     }
 }
